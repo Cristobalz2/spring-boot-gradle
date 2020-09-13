4d90f5fe44e4252701190973ef09d741172f5217
devopstrap <devopstrap@localhost.localdomain>
2020-09-13 09:50:16 -0500

    final push

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "DevCO") String name) {
 		return String.format("Hello %s!!!", name);
 	}
 }
